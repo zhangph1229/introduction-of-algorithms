73aa2333dea406a9a506ffa837a6c61256935372
zhangph1229 <zhangph1229@163.com>
2016-06-28 16:56:35 +0800

    传输文件

---
+++ src/main/java/cn/edu/neu/zhangph/ch4/Test.java
@@ -0,0 +1,9 @@
+package cn.edu.neu.zhangph.ch4;
+
+import java.io.*;
+public class Test {
+	public static void main(String[] args){ 
+		File file = new File("pic/");
+		System.out.println(file.getAbsolutePath());
+	}
+}
